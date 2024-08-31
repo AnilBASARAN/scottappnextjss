ee9ec6a6a7e53463a9d577c6f59b0efd3b60ed7a
Anıl Başaran <nlbsrn@gmail.com>
2024-08-31 10:26:10 +0300

    revalidate path to fetch the data without fetching it

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,6 @@
 'use server'
 import db from './db'
+import { revalidatePath } from 'next/cache'
 
 
 export const newTodo = async (formData) => {
@@ -8,4 +9,6 @@ export const newTodo = async (formData) => {
         content: formData.get('content'),
     },
   })
+
+  revalidatePath('/todos')
 }
